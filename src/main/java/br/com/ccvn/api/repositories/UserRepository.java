3c74709957f4af3e3520e388ad9a445d097a596b
wanderson-sone <wandersonslisboa@gmail.com>
2021-02-03 01:28:10 -0300

    Criação do UserRepository

---
+++ src/main/java/br/com/ccvn/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.ccvn.api.repositories;
+
+import br.com.ccvn.api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
