8642860f6879081f6f83c9ec6ef0002916b23493
wanderson-sone <wandersonslisboa@gmail.com>
2021-02-02 23:14:16 -0300

    criação do projeto

---
+++ src/main/java/br/com/ccvn/api/CcvnApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ccvn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcvnApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcvnApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ccvn/api/CcvnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ccvn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcvnApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
